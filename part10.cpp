85483ea9e99e7105438056d9df99a8dc173e62a1
Riddhidipta Pal <riddhidipta2003@gmail.com>
2022-12-07 21:48:48 +0530

    2

--- part10.cpp
+++ part10.cpp
@@ -3,3 +3,16 @@ using namespace std;
 
 //function overloading
 
+int sum(int a, int b, int c){
+    return a+b+c;
+}
+
+int sum(int a, int b){
+    return a+b;
+}
+
+int main(){
+    cout<<sum(2+4)<<endl;
+    cout<<sum(2+4+5)<<endl;
+    return 0;
+}

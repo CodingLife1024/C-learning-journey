83892d23a6517195be521097c414f4679f37c4a2
Riddhidipta Pal <riddhidipta2003@gmail.com>
2022-12-12 21:58:30 +0530

    g

--- part12.cpp
+++ part12.cpp
@@ -1,6 +1,65 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
 
+class binary
+{
+    string s;
+
+public:
+    void read(void);
+    void chk_bin(void);
+    void ones(void);
+    void display(void);
+};
+
+void binary ::read(void)
+{
+    cout << "enter a binary number: ";
+    cin >> s;
+}
+
+void binary ::chk_bin(void)
+{
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s.at(i) != '0' && s.at(i) == '1')
+        {
+            cout << "incorrect binary format" << endl;
+            exit(0);
+        }
+    }
+}
+
+void binary ::ones(void)
+{
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s.at(i) == '0')
+        {
+            s.at(i) = '1';
+        }
+        else{
+            s.at(i) = '0';
+        }
+    }
+}
+
+void binary :: display(void)
+{
+    cout<<"displaying the binary number: ";
+    for (int i = 0; i < s.length(); i++)
+    {
+        cout<<s.at(i);
+    }
+    cout<<endl;
+}
+
 int main(){
+    binary b;
+    b.read();
+    b.display();
+    b.ones();
+    b.display();
     return 0;
 }

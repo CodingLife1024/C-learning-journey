8b89097a70f5e74b72d9656e16ac1a5fd89b9f4d
Riddhidipta Pal <riddhidipta2003@gmail.com>
2022-12-15 01:15:34 +0530

    g

--- part17.cpp
+++ part17.cpp
@@ -1,11 +1,35 @@
 #include<iostream>
 using namespace std;
 
+class Y;
+
 class X{
     int data;
-    
+    public:
+        void setValue(int value){
+            data = value;
+        }
+    friend void add(X, Y);
 };
 
+class Y{
+    int num;
+    public:
+        void setValue(int value){
+            num = value;
+        }
+    friend void add(X, Y);
+};
+
+void add(X o1, Y o2){
+    cout<<"Summing gives: "<<(o1.data + o2.num)<<endl;
+}
+
 int main(){
+    X a;
+    a.setValue(3);
+    Y b;
+    b.setValue(5);
+    add(a, b);
     return 0;
 }

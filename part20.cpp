5cf48e5745ae9840a45fcdf0fed51d3cf25bcb6b
Riddhidipta Pal <riddhidipta2003@gmail.com>
2022-12-15 23:17:11 +0530

    g

--- part20.cpp
+++ part20.cpp
@@ -1,6 +1,24 @@
 #include<iostream>
 using namespace std;
 
+class Complex{
+    int a, b;
+    public:
+        void printData(){
+            cout << "The number is " << a << " + " << b << "i" << endl;
+        }
+    Complex(int, int);
+};
+
+Complex ::Complex(int x, int y){
+    a = x;
+    b = y;
+};
+
 int main(){
+    Complex a(3, 5);
+    Complex b = Complex(7, 8);
+    a.printData();
+    b.printData();
     return 0;
 }

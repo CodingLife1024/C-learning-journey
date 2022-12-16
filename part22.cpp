72957780156c47f227c61cabc54a7800623519fa
Riddhidipta Pal <riddhidipta2003@gmail.com>
2022-12-16 22:48:44 +0530

    g

---
+++ part22.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+class bankDeposit{
+    int principle;
+    int years;
+    float interestRate;
+    float returnValue;
+};
+
+int main(){
+    return 0;
+}

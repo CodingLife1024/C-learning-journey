78ee303a9ba9f9bfbbf56e7e0fde8a5ca13d3a00
Riddhidipta Pal <riddhidipta2003@gmail.com>
2022-12-17 00:40:06 +0530

    g

--- part23.cpp
+++ part23.cpp
@@ -2,6 +2,29 @@
 #include<cmath>
 using namespace std;
 
+int count = 0;
+class num{
+    public:
+        num(){
+            count++;
+            cout<<"This is the time when constructor is called for object number  "<<count<<endl;
+        }
+        ~num(){
+            cout<<"This is the time when destructor is called for object number  "<<count<<endl;
+            count--;
+        }
+};
+
 int main(){
+    cout<<"We are inside our main function."<<endl;
+    cout<<"Creating first object n1."<<endl;
+    num n1;
+    {
+        cout<<"Entering this block."<<endl;
+        cout<<"Creating 2 more objects, n2 and n3"<<endl;
+        num n2, n3;
+        cout<<"Exiting this block."<<endl;
+    }
+    cout<<"Back to main."<<endl;
     return 0;
 }

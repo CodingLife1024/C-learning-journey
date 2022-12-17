1ae8512295247628d27e78199496260a31bdfe68
Riddhidipta Pal <riddhidipta2003@gmail.com>
2022-12-17 21:24:30 +0530

    g

--- part24.cpp
+++ part24.cpp
@@ -2,6 +2,22 @@
 #include<cmath>
 using namespace std;
 
+//Inheritance
+
+//Base Class
+class Employee{
+    int id;
+    float salary;
+};
+
+//Derived class syntax
+/*
+class {{derived-class-name}} : {{visibility mode}} {{base-class-name}}
+{
+    class members/methods etc
+}
+*/
+
 int main(){
     return 0;
 }

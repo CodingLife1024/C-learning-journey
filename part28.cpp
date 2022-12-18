1acea476a6a9eb525aff1c46769ae21e6502bcb2
Riddhidipta Pal <riddhidipta2003@gmail.com>
2022-12-18 23:43:59 +0530

    g

--- part28.cpp
+++ part28.cpp
@@ -2,6 +2,67 @@
 #include<cmath>
 using namespace std;
 
+//Syntax for multiple inheritance
+/*
+class DerivedC: visibility-mode base-1, visibility-mode base-2
+{
+    class body of class "DerivedC"
+};
+*/
+
+class Base1{
+    protected:
+        int base1int;
+    public:
+        void set_base1int(int a){
+            base1int = a;
+        }
+};
+
+class Base2{
+    protected:
+        int base2int;
+    public:
+        void set_base2int(int a){
+            base2int = a;
+        }
+};
+
+class Base3{
+    protected:
+        int base3int;
+    public:
+        void set_base3int(int a){
+            base3int = a;
+        }
+};
+
+class Derived: public Base1, public Base2, public Base3{
+    public:
+        void show(){
+            cout<<"The value of Base1 is: "<<base1int<<endl;
+            cout<<"The value of Base2 is: "<<base2int<<endl;
+            cout<<"The value of Base3 is: "<<base3int<<endl;
+            cout<<"The value sum is: "<<(base1int+base2int+base3int)<<endl;
+        }
+};
+
+/*
+The inherited generated derived class looks like:
+Data members:
+    base1int --> protected
+    base2int --> protected
+Member functions:
+    set_base1int(int) --> public
+    set_base1int(int) --> public
+    show() --> public
+*/
+
 int main(){
+    Derived hrry;
+    hrry.set_base1int(22);
+    hrry.set_base2int(45);
+    hrry.set_base3int(4);
+    hrry.show();
     return 0;
 }

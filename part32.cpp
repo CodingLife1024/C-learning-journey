63b34c6d1454d743ed91e0a1cf98a29b7f319be4
Riddhidipta Pal <riddhidipta2003@gmail.com>
2022-12-20 19:30:20 +0530

    g

--- part32.cpp
+++ part32.cpp
@@ -2,6 +2,24 @@
 #include<cmath>
 using namespace std;
 
+/*
+Syntax for Initialisation List in Constructor:
+constructor (argument-list): initialisation section
+{
+    assignment + other code;
+}
+*/
+
+class Test{
+    int a, b;
+    public:
+        Test(int i, int j): b(j), a(i + b){
+            cout<<"Constructor done"<<endl;
+            cout<<"The values are "<<a<<" and "<<b<<endl;
+        }
+};
+
 int main(){
+    Test(5, 7);
     return 0;
 }

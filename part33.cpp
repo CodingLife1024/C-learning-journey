17f506577964bc81418a45f343c03f95ec20e9c3
Riddhidipta Pal <riddhidipta2003@gmail.com>
2022-12-20 21:25:23 +0530

    g

--- part33.cpp
+++ part33.cpp
@@ -2,6 +2,19 @@
 #include<cmath>
 using namespace std;
 
+//Revisiting pointers
+
 int main(){
+    int a = 4;
+    int* (ptr) = &a;
+    cout<<"The value of a is "<<*(ptr)<<endl;
+    float* p = new float(6.0976);
+    cout<<"The value at address p is "<<*(p)<<endl;
+    int* arr = new int[3];
+    arr[0] = 10;
+    *(arr+ 1) = 20;
+    arr[2] = 30;
+    //delete arr;
+    cout<<"The values are "<<arr[0]<<", "<<arr[1]<<" and "<<arr[2]<<endl;
     return 0;
 }

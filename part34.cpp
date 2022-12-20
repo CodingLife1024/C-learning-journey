84fad4b54bfab0544d5487df39cc4a67bb61d141
Riddhidipta Pal <riddhidipta2003@gmail.com>
2022-12-20 21:45:43 +0530

    g

--- part34.cpp
+++ part34.cpp
@@ -2,6 +2,26 @@
 #include<cmath>
 using namespace std;
 
+class Complex{
+    int real, imaginary;
+    public:
+        void getData(){
+            cout<<"The real part: "<<real<<endl;
+            cout<<"The imaginary part: "<<imaginary<<endl;
+        }
+        void setData(int a, int b){
+            real = a;
+            imaginary = b;
+        }
+};
+
 int main(){
-    return 0;
+    // Complex c1;
+    // Complex *ptr = &c1;
+    // (*ptr).setData(2, 77);
+    // (*ptr).getData();
+    Complex *(ptr) = new Complex;
+    ptr -> setData(45, 78);
+    //(*ptr).getData();
+    ptr -> getData();
 }

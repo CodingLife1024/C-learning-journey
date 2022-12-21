5e87e613488388f6f673a4531c5d2396a2e045c5
Riddhidipta Pal <riddhidipta2003@gmail.com>
2022-12-21 22:26:36 +0530

    g

--- part37.cpp
+++ part37.cpp
@@ -4,7 +4,22 @@ using namespace std;
 
 //Polymorphism
 //one name- multiple forms
+//2 types of polymorphism- compile-time and run-time
+//2 types of compile-time polymorphism- function-overloading and operator-overloading
+//run-time polymorphism- virtual-funtctions
+
+class baseClass{
+
+};
+
+class derivedClass: public baseClass{
+
+};
 
 int main(){
+    baseClass* base_class_pointer;
+    baseClass obj_base;
+    derivedClass obj_derived;
+    base_class_pointer = &obj_derived;
     return 0;
 }

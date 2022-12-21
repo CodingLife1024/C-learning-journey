c420fc4068b0ce63ef425a0df0d61855e8d21551
Riddhidipta Pal <riddhidipta2003@gmail.com>
2022-12-21 22:45:23 +0530

    g

--- part37.cpp
+++ part37.cpp
@@ -9,11 +9,20 @@ using namespace std;
 //run-time polymorphism- virtual-funtctions
 
 class baseClass{
-
+    public:
+        int var_base;
+        void display(){
+            cout<<"Displaying base class variable "<<var_base<<endl;
+        }
 };
 
 class derivedClass: public baseClass{
-
+    public:
+        int var_derived;
+        void display(){
+            cout<<"Displaying base class variable "<<var_base<<endl;
+            cout<<"Displaying derived class variable "<<var_derived<<endl;
+        }
 };
 
 int main(){
@@ -21,5 +30,18 @@ int main(){
     baseClass obj_base;
     derivedClass obj_derived;
     base_class_pointer = &obj_derived;
+
+    base_class_pointer -> var_base = 23;
+    base_class_pointer -> display();
+
+    base_class_pointer -> var_base = 343;
+    base_class_pointer -> display();
+
+    derivedClass* derived_class_pointer;
+    derived_class_pointer = &obj_derived;
+    derived_class_pointer -> var_derived = 76;
+    derived_class_pointer -> var_base = 6;
+    derived_class_pointer -> display();
+
     return 0;
 }

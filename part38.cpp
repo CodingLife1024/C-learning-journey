37893fb37e7f5b00766133d9424dfe9f3a434f92
Riddhidipta Pal <riddhidipta2003@gmail.com>
2022-12-21 23:13:59 +0530

    g

--- part38.cpp
+++ part38.cpp
@@ -2,6 +2,29 @@
 #include<cmath>
 using namespace std;
 
+class baseClass{
+    public:
+        int var_base;
+        void display(){
+            cout<<"1. Displaying base class variable "<<var_base<<endl;
+        }
+};
+
+class derivedClass: public baseClass{
+    public:
+        int var_derived;
+        void display(){
+            cout<<"2. Displaying base class variable "<<var_base<<endl;
+            cout<<"2. Displaying derived class variable "<<var_derived<<endl;
+        }
+};
+
 int main(){
+    baseClass* base_class_pointer;
+    baseClass obj_base;
+    derivedClass obj_derived;
+
+    base_class_pointer = &obj_derived;
+    base_class_pointer -> display();
     return 0;
 }

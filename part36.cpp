c5063d1b2f18a9633b6f78a48f89082d9eeaa465
Riddhidipta Pal <riddhidipta2003@gmail.com>
2022-12-21 21:44:34 +0530

    g

--- part36.cpp
+++ part36.cpp
@@ -2,6 +2,24 @@
 #include<cmath>
 using namespace std;
 
+class A{
+    int a;
+    public:
+        // void setData(int a){
+        //     this -> a = a;
+        // }
+        A& setData(int a){
+            this -> a = a;
+            return *this;
+        }
+        void getData(){
+            cout<<"The value is "<<a<<endl;
+        }
+};
+
 int main(){
+    A a;
+    a.setData(77).getData();
+    a.getData();
     return 0;
 }

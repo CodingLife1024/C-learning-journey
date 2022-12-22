6e64cda66917122d921cf017d301615f73e711fd
Riddhidipta Pal <riddhidipta2003@gmail.com>
2022-12-22 22:05:26 +0530

    g

--- part39.cpp
+++ part39.cpp
@@ -34,11 +34,11 @@ class CWHText: public CWH{
         CWHText(string s, float r, int wc): CWH(s, r){
             words = wc;
         }
-     void display(){
-      cout<<"This is an amazing text tutorial with title "<<title<<endl;
-      cout<<"Ratings of this text tutorial: "<<rating<<" out of 5 stars"<<endl;
-      cout<<"No of words in this text tutorial is: "<<words<<" words"<<endl;
-         }
+    void display(){
+        cout<<"This is an amazing text tutorial with title "<<title<<endl;
+        cout<<"Ratings of this text tutorial: "<<rating<<" out of 5 stars"<<endl;
+        cout<<"No of words in this text tutorial is: "<<words<<" words"<<endl;
+    } 
 };
 
 int main(){

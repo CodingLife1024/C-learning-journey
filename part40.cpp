5000b30352167a855869d6e944e3f6ef1a94d119
Riddhidipta Pal <riddhidipta2003@gmail.com>
2022-12-22 22:43:15 +0530

    g

--- part40.cpp
+++ part40.cpp
@@ -2,6 +2,17 @@
 #include<fstream>
 using namespace std;
 
+/*
+The useful classes for working with files in C++:
+1. fstreambase
+2. ifstream --> derived from fstreambase
+3. ofstream --> derived from fstreambase
+
+Two ways to open a file:
+1. using the constructor
+2. using the member function open() for the class
+*/
+
 int main(){
     return 0;
 }

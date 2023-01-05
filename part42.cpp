e13bb5013d7cda0f16c34f7c47bef251c2776995
Riddhidipta Pal <riddhidipta2003@gmail.com>
2023-01-05 21:14:50 +0530

    g

--- part42.cpp
+++ part42.cpp
@@ -1,7 +1,36 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
 using namespace std;
+ 
+class vector
+{
+    public:
+        int *arr;
+        int size;
+        vector(int m)
+        {
+            size = m;
+            arr = new int[size];
+        }
+    int dotProduct(vector &v){
+        int d=0;
+        for (int i = 0; i < size; i++)
+        {
+            d+=this->arr[i]*v.arr[i];
+        }
+        return d;
+    }
+};
 
 int main(){
+    vector v1(3); //vector 1
+    v1.arr[0] = 4;
+    v1.arr[1] = 3;
+    v1.arr[2] = 1;
+    vector v2(3); //vector 2
+    v2.arr[0]=1;
+    v2.arr[1]=0;
+    v2.arr[2]=1;
+    int a = v1.dotProduct(v2);
+    cout<<a<<endl;
     return 0;
 }

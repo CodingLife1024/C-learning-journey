b2aefd8916b011af98dc8651b3b6da90def04d1c
Riddhidipta Pal <riddhidipta2003@gmail.com>
2023-01-06 23:39:55 +0530

    g

--- part46.cpp
+++ part46.cpp
@@ -1,7 +1,27 @@
 #include<iostream>
-#include<cmath>
 using namespace std;
+template<class T3>
+void swapp(T3 &a, T3 &b){
+    T3 temp = a;
+    a = b;
+    b = temp;
+};
+
+template<class T1, class T2>
+float funcAverage(T1 a, T2 b){
+    float avg= (a+b)/2.0; 
+    return avg;
+}
 
 int main(){
+    float a;
+    a = funcAverage(2, 5.2);
+    cout<<"the avg is: "<<a<<endl;
+    printf("The average of these numbers is %f",a);
+    cout<<endl;
+    int x = 4, y = 9;
+    cout<<x<<y<<endl;
+    swapp(x, y);
+    cout<<x<<y<<endl;
     return 0;
 }
--- part47.cpp
+++ part47.cpp
@@ -2,6 +2,39 @@
 #include<cmath>
 using namespace std;
 
+template<class T>
+class Har{
+    public:
+        T data;
+        Har(T a){
+            data = a;
+        }
+        void display();
+        //{
+        //     cout<<data<<endl;
+        // }
+};
+
+template<class T>
+void Har<T>:: display(){
+    cout<<data<<endl;
+};
+
+void func(int a){
+    cout<<"I am first func() "<<a<<endl;
+}
+ 
+template<class T>
+void func1(T a){
+    cout<<"I am templatised func() "<<a<<endl;
+}
+
 int main(){
+    // Har<float> h (5.77);
+    // cout<<h.data<<endl;
+    // h.display();
+
+    func(4);
+    func1(6);
     return 0;
 }
